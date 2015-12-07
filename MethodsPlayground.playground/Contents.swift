07fe4b35ac32df3db746444320cdbfe3df7295b6
Ethan throndsen <Ethunderson1@gmail.com>
2015-12-07 12:34:49 -0700

    Fix spelling on playground code

--- MethodsPlayground.playground/Contents.swift
+++ MethodsPlayground.playground/Contents.swift
@@ -24,3 +24,35 @@ func isTired () -> Bool
 print(isTired())
 
 
+func checkValue(someNumber :Int) -> String
+{
+    var answer :String = ""
+    
+    if (someNumber < -5)
+    {
+        answer = "that is a small number"
+    }
+    else if (someNumber == 0)
+    {
+        answer = "the first even number 🇳🇴"
+    }
+    else
+    {
+        answer = "that number is not too small"
+    }
+    
+    return answer
+}
+//Java Ver
+//checkValue(4);
+print(checkValue(0))
+
+func multiParameterMhod(words :String, age :Int, values : Doubel) -> String
+{
+    var cobineString :String = ""
+    co0mbinedString = woreds + " " + /(age) + " " /(values)
+    retrun combinedString
+}
+print(multiParametermethod("Ethan", age: 18, clues: 1234))
+print(multiparamterethod("woeds for calss", age 6,vlaues: 30.1))
+
