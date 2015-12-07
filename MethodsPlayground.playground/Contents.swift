7cf8a3a500dbb2f12af8eb2fb1b29ca40691a456
Ethan throndsen <Ethunderson1@gmail.com>
2015-12-07 11:58:35 -0700

    Added Methods and compared to java methods

---
+++ MethodsPlayground.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var title = "Using methods in swift"
+//Java Ver
+// public void doesNothing()
+//{
+//
+//}
+func doesNothing() -> Void
+{
+        print("Boring")
+}
+// Java Boolean method
+//public boolean isTired()
+//{
+//  return False;
+//}
+func isTired () -> Bool
+{
+    return false
+}
+print(isTired())
+
+
